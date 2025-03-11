b6df0cf6e666412262d8dd2b4136c1b24bbb8668
hoit1011 <parksm071011@gmail.com>
2025-03-11 09:47:43 +0900

    Initial Commit

---
+++ walkingGO/walkingGOApp.swift
@@ -0,0 +1,17 @@
+//
+//  walkingGOApp.swift
+//  walkingGO
+//
+//  Created by 박성민 on 3/11/25.
+//
+
+import SwiftUI
+
+@main
+struct walkingGOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ walkingGOTests/walkingGOTests.swift
@@ -0,0 +1,17 @@
+//
+//  walkingGOTests.swift
+//  walkingGOTests
+//
+//  Created by 박성민 on 3/11/25.
+//
+
+import Testing
+@testable import walkingGO
+
+struct walkingGOTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}

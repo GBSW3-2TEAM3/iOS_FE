589c9c2c32c73b21cf5107ddb3cfcb52b0eb1e14
hoit1011 <parksm071011@gmail.com>
2025-04-22 15:51:26 +0900

    [Feature] #8 teamPageView 퍼블리싱
    변경 사항
    - TeamPageView 퍼블리싱
    - CustomTextEditor viewModifier구현

--- walkingGO/Source/Component/CustomButton.swift
+++ walkingGO/Source/Component/CustomButton.swift
@@ -10,14 +10,22 @@ import SwiftUI
 struct CustomButton: View {
     var title: String
     var action: () -> Void
+    var width: CGFloat = 300
+    var textColor: Color = .white
+    var backgroundColor: Color = .customBlue
+    var strokeColor: Color = .white
     var body: some View {
         Button(action:action){
             Text(title)
-                .frame(width: 300,height: 50)
+                .frame(width: width,height: 50)
                 .font(AppFont.PretendardBold(size: 15))
-                .foregroundStyle(.white)
-                .background(.customBlue)
+                .foregroundStyle(textColor)
+                .background(backgroundColor)
                 .clipShape(RoundedRectangle(cornerRadius: 13))
+                .overlay(
+                    RoundedRectangle(cornerRadius: 13)
+                        .stroke(strokeColor, lineWidth: 1)
+                )
         }
     }
 }
@@ -25,6 +33,9 @@ struct CustomButton: View {
 #Preview {
     CustomButton(
         title:"로그인",
-        action: {}
+        action: {},
+        textColor: .blue,
+        backgroundColor: .white,
+        strokeColor: .black
     )
 }
